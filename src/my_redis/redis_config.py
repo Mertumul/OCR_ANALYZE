ac7c123832e5d4ae7e2ccc479671b21abeea43cd
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2023-08-20 23:21:21 +0000

    Add files via upload

---
+++ src/my_redis/redis_config.py
@@ -0,0 +1,8 @@
+import redis
+from dynaconf import Dynaconf
+settings = Dynaconf(settings_file="settings.toml")
+
+REDIS_HOST = settings.redis.host
+REDIS_PORT = int(settings.redis.port)
+
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
